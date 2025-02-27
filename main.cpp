398d289a2f2e5debd653791bd76d0b8bfc67db0f
treslecheson <146301502+treslecheson@users.noreply.github.com>
2025-02-27 11:32:40 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,17 @@
 #include <system_error>
 using namespace std;
 
+
+string help = "--help";
+string view = "view";
+string add  = "add";
+
 string green = "\e[0;32m";
 string red = "\e[0;31m";
+string blue = "\e[0;34m";
+string underlinePurple = "\e[4;35m";
+
+
 
 string startsWith(string symbol, string phrase){
     if(phrase.find(symbol) == 0){
@@ -34,6 +43,12 @@ string icons(string symbol, string phrase){
     }else if(starting == "."){
         format(red, "●",new_phrase);
         return "●"+new_phrase;
+    }else if(starting == "*"){
+        format(blue,"💡", new_phrase);
+        return "💡"+new_phrase;
+    }else if(starting == "#"){
+        format(underlinePurple, "", new_phrase);
+        return new_phrase; 
     }
     return new_phrase;
 }
@@ -57,12 +72,15 @@ void viewFile(string file){
     while(getline(inputFile, line)){
         icons(".", line);
         icons("/", line);
+        icons("*", line);
+        icons("#", line);
     }
 }
 
 void helpinfo(){
     cout<<"Usage:"<<'\n';
-    cout<<"  projgo [command]"<<'\n';
+    cout<<"proj"<<'\n';
+    cout<<"  proj [command]"<<'\n';
     cout<<'\n';
     cout<<"Available Commands:"<<'\n';
     cout<<"  view	view the file"<<'\n';
@@ -71,19 +89,36 @@ void helpinfo(){
     cout<<"  --help, -h	    help for projgo"<<'\n';
 }
 
+void runMode(){
+    int i = 0;
+    while(i < 1){
+        string command;
+        cout<<"Enter Command: ";
+        cin >> command;
+
+        if(command == help){
+            helpinfo();
+        }else if(command == view){
+            viewFile("proj.txt");
+        }else if(command == add){
+            string message;
+            cout<<"What would you like to write?";
+            getline(cin, message);
+            write("proj.txt", message);
+        }
+    }
+}
+
 int main(int argc, char* argv[]){
     int i = 0;
-    string help = "--help";
-    string view = "view";
-    string add  = "add";
     if(argc == 2 and argv[1] == help){
         helpinfo();
     }else if(argc == 2 and argv[1] == view){
-        viewFile("test.txt");
+        viewFile("proj.txt");
     }else if(argc == 3 and argv[1] == add){
         string message = argv[2];
-
-        write("test.txt", message);
+        write("proj.txt", message);
+    }else if(argc == 1){
+        runMode();
     }
-
 }
